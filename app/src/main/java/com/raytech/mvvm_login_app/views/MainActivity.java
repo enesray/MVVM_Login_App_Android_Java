d5bb13280131f1757e96e8e2af180e700096f372
enesray <iletisim.enesray@gmail.com>
2023-02-01 14:12:08 +0300

    First commit

---
+++ app/src/main/java/com/raytech/mvvm_login_app/views/MainActivity.java
@@ -0,0 +1,31 @@
+package com.raytech.mvvm_login_app.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.raytech.mvvm_login_app.*;
+
+import com.raytech.mvvm_login_app.databinding.ActivityMainBinding;
+import com.raytech.mvvm_login_app.viewmodels.LoginViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
